aac4ee8e29938ff06f87dccbd50985b47a3406f3
fender13 <ej.fender@outlook.com>
2019-04-22 18:03:57 +0700

    feature login register untuk set username DONE

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="app">
-    <router-view/>
+    <MainNavbar></MainNavbar>
+    <div class="main">
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view/>
+      </transition>
+    </div>
   </div>
 </template>
 
+<script>
+import MainNavbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MainNavbar
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  max-width: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
 }
 </style>
